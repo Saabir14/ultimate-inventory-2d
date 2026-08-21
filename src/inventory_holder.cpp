4566da3040b02c932b24fde3222a4a7a6576b64f
Saabir Ahmed <64490729+Saabir14@users.noreply.github.com>
2026-08-21 17:51:39 +0300

    added property hint

--- src/inventory_holder.cpp
+++ src/inventory_holder.cpp
@@ -1,4 +1,5 @@
 #include "inventory_holder.hpp"
+#include "godot_cpp/classes/global_constants.hpp"
 #include "godot_cpp/classes/ref.hpp"
 #include "inventory.hpp"
 
@@ -7,7 +8,7 @@ using namespace godot;
 void InventoryHolder::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_inventory", "inventory"), &InventoryHolder::set_inventory);
 	ClassDB::bind_method(D_METHOD("get_inventory"), &InventoryHolder::get_inventory);
-	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "inventory"), "set_inventory", "get_inventory");
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "inventory", PROPERTY_HINT_RESOURCE_TYPE, "Inventory"), "set_inventory", "get_inventory");
 }
 
 void InventoryHolder::set_inventory(const Ref<Inventory> &p_inventory) { inventory = p_inventory; }
