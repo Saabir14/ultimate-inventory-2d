13b0cf8f890cbdfff6ccd7296203fce51029da5d
Saabir Ahmed <64490729+Saabir14@users.noreply.github.com>
2026-08-22 22:16:52 +0300

    fixed property binding

--- src/resource/inventory.cpp
+++ src/resource/inventory.cpp
@@ -7,7 +7,7 @@ using namespace godot;
 void Inventory::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_slots", "slots"), &Inventory::set_slots);
 	ClassDB::bind_method(D_METHOD("get_slots"), &Inventory::get_slots);
-	ADD_PROPERTY(PropertyInfo(Variant::ARRAY, "slots", PROPERTY_HINT_ARRAY_TYPE, "InventoryItem", PROPERTY_USAGE_DEFAULT, "Ref<InventoryItem>"), "set_items", "get_items");
+	ADD_PROPERTY(PropertyInfo(Variant::ARRAY, "slots", PROPERTY_HINT_ARRAY_TYPE, "InventorySlot", PROPERTY_USAGE_DEFAULT, "Ref<InventoryItem>"), "set_slots", "get_slots");
 }
 
 void Inventory::set_slots(const TypedArray<Ref<InventorySlot>> &p_slots) { slots = p_slots; }
