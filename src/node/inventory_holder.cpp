a87a3c27e5634e4dd6b7f8cc07baf5d9a18491a2
Saabir Ahmed <64490729+Saabir14@users.noreply.github.com>
2026-09-14 12:26:07 +0300

    added an attached_node for the inventory holder to attach to

--- src/node/inventory_holder.cpp
+++ src/node/inventory_holder.cpp
@@ -9,11 +9,44 @@
 using namespace godot;
 
 void InventoryHolder::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("get_attached_node"), &InventoryHolder::get_attached_node);
+	ClassDB::bind_method(D_METHOD("set_attached_node", "node"), &InventoryHolder::set_attached_node);
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "attached_node", PROPERTY_HINT_NODE_TYPE, "Node"), "set_attached_node", "get_attached_node");
+
+	ClassDB::bind_method(D_METHOD("get_use_parent_as_attached_node"), &InventoryHolder::get_use_parent_as_attached_node);
+	ClassDB::bind_method(D_METHOD("set_use_parent_as_attached_node", "use_parent"), &InventoryHolder::set_use_parent_as_attached_node);
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "use_parent_as_attached_node"), "set_use_parent_as_attached_node", "get_use_parent_as_attached_node");
+
 	ClassDB::bind_method(D_METHOD("get_inventory"), &InventoryHolder::get_inventory);
 	ClassDB::bind_method(D_METHOD("set_inventory", "inventory"), &InventoryHolder::set_inventory);
 	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "inventory", PROPERTY_HINT_RESOURCE_TYPE, "Inventory"), "set_inventory", "get_inventory");
 
 	ADD_SIGNAL(MethodInfo("inventory_set", PropertyInfo(Variant::OBJECT, "inventory", PROPERTY_HINT_RESOURCE_TYPE, "Inventory")));
+}
+
+void InventoryHolder::set_attached_node(Node *p_node) {
+	if (attached_node == p_node)
+		return;
+
+	attached_node = p_node;
+	use_parent_as_attached_node = false;
+}
+Node *InventoryHolder::get_attached_node() const { return attached_node; }
+
+void InventoryHolder::set_use_parent_as_attached_node(bool p_use_parent) {
+    use_parent_as_attached_node = p_use_parent;
+    if (p_use_parent)
+        attached_node = get_parent();
+}
+bool InventoryHolder::get_use_parent_as_attached_node() const { return use_parent_as_attached_node; }
+
+void InventoryHolder::_notification(int p_what) {
+    switch (p_what) {
+        case NOTIFICATION_PARENTED:
+            if (use_parent_as_attached_node)
+                attached_node = get_parent();
+            break;
+    }
 }
 
 void InventoryHolder::set_inventory(const Ref<Inventory> &p_inventory) {
--- src/node/inventory_holder.hpp
+++ src/node/inventory_holder.hpp
@@ -8,12 +8,22 @@ class InventoryHolder : public Node {
 	GDCLASS(InventoryHolder, Node)
 
 private:
+	Node *attached_node;
+	bool use_parent_as_attached_node = true;
+
 	Ref<Inventory> inventory;
 
 protected:
 	static void _bind_methods();
+	void _notification(int p_what);
 
 public:
+	void set_attached_node(Node *p_node);
+	Node *get_attached_node() const;
+
+	void set_use_parent_as_attached_node(bool p_use_parent);
+	bool get_use_parent_as_attached_node() const;
+
 	void set_inventory(const Ref<Inventory> &p_inventory);
 	Ref<Inventory> get_inventory() const;
 };
