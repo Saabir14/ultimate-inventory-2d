eabfca78d83a647de18d04242b6396209df20512
Saabir Ahmed <64490729+Saabir14@users.noreply.github.com>
2026-09-18 12:34:09 +0300

    default item_ui_holder is self

--- src/node/slot_ui.cpp
+++ src/node/slot_ui.cpp
@@ -44,20 +44,21 @@ Ref<InventoryItem> SlotUI::get_item() const {
 }
 
 void SlotUI::set_item_ui_holder(Node *p_holder) {
-	if (!p_holder)
-		item_ui_holder = this;
+	if (item_ui_holder == p_holder)
+		return;
 
-	item_ui_holder = p_holder;
+	if (item_ui_holder)
+		item_ui_holder->remove_child(item_ui);
+
+	item_ui_holder = p_holder ? p_holder : this;
 	_update_ui();
 }
 Node *SlotUI::get_item_ui_holder() const { return item_ui_holder; }
 
 void SlotUI::_update_ui() {
-	Node *holder = item_ui_holder ? item_ui_holder : this;
-
 	// Free item ui
 	if (item_ui) {
-		holder->remove_child(item_ui);
+	    item_ui_holder->remove_child(item_ui);
 		item_ui->queue_free();
 		item_ui = nullptr;
 	}
@@ -75,7 +76,7 @@ void SlotUI::_update_ui() {
 		return;
 
 	// Add item ui as child of item ui holder
-	holder->add_child(item_ui, false);
+	item_ui_holder->add_child(item_ui, false);
 }
 
 Variant SlotUI::_get_drag_data(const Vector2 &p_position) {
